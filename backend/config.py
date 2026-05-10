e9daa9773f9ab041d4b07599d68f16468f7d8185
Romanencocmd <ramal.safarov.05@mail.ru>
2026-05-10 16:24:19 +0200

    Load environment variables for configuration

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
-    SECRET_KEY = "supersecretkey"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SESSION_TYPE = "filesystem"
